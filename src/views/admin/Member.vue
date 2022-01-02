fb304bb4b236524ef833c62b644c2373333809a8
Palcimer <palcimer@gmail.com>
2022-01-02 17:43:28 +0900

    22/01/02 회원관리자 회원정보수정/탈퇴 처리, 탈퇴회원 구분, 페이지네이션

--- src/views/admin/Member.vue
+++ src/views/admin/Member.vue
@@ -3,6 +3,26 @@
     <v-toolbar>
       <v-toolbar-title>회원 관리</v-toolbar-title>
       <search-field :items="searchItems" :options.sync="options" />
+      <v-spacer></v-spacer>
+      <v-sheet width="100" class="mt-2">
+        <v-select
+          :items="mTypeItems"
+          v-model="options.mType"
+          label="회원유형"
+          hide-details
+          @change="options.page = 1"
+          dense
+        />
+      </v-sheet>
+      <v-sheet width="100" class="mt-2 ml-4">
+        <v-select
+          :items="pageItems"
+          v-model="options.itemsPerPage"
+          label="페이지 당 행 수"
+          hide-details
+          dense
+        />
+      </v-sheet>
     </v-toolbar>
     <v-data-table
       :headers="headers"
@@ -10,20 +30,54 @@
       :options.sync="options"
       :server-items-length="totalItems"
       :loading="loading"
-    ></v-data-table>
+      hide-default-footer
+    >
+      <template v-slot:item.cmd="{ item }">
+        <tooltip-btn icon label="수정" @click="openDialog(item)">
+          <v-icon>mdi-pencil</v-icon>
+        </tooltip-btn>
+      </template>
+    </v-data-table>
+
+    <v-pagination
+        v-model="options.page"
+        :length="pageCount"
+        class="mt-4"
+      ></v-pagination>
+
+    <ez-dialog
+      label="회원 수정"
+      ref="dialog"
+      width="500"
+      persistent
+      @onClose="dialogClose"
+    >
+      <user-update-form
+        v-if="currMember"
+        :member="currMember"
+        :isLoading="loading"
+        :admMode="true"
+        @onSave="saveMember"
+        @onLeave="leaveMember"
+      />
+    </ez-dialog>
   </v-container>
 </template>
 
 <script>
 import qs from "qs";
 import SearchField from "../../components/layout/SearchField.vue";
+import TooltipBtn from "../../components/etc/TooltipBtn.vue";
 import axios from "axios";
+import EzDialog from "../../components/etc/EzDialog.vue";
+import UserUpdateForm from "../../components/Auth/UserUpdateForm.vue";
+import { deepCopy } from '../../../util/lib';
 export default {
-  components: { SearchField },
+  components: { SearchField, TooltipBtn, EzDialog, UserUpdateForm },
   name: "admMember",
   data() {
     return {
-      headers: [
+      headersOrigin: [
         {
           text: "아이디",
           value: "mb_id",
@@ -54,6 +108,16 @@ export default {
           align: "start",
         },
         {
+          text: "수정일",
+          value: "mb_update_at",
+          align: "start",
+        },
+        {
+          text: "탈퇴일",
+          value: "mb_leave_at",
+          align: "start",
+        },
+        {
           text: "CMD",
           value: "cmd",
           sortable: false,
@@ -66,12 +130,35 @@ export default {
       pageReady: false,
       pageRouting: false,
       axiosSource: null,
+      currMember: null,
+      mTypeItems: [
+        {
+          text: "회원",
+          value: "member",
+        },
+        {
+          text: "탈퇴회원",
+          value: "left",
+        },
+      ],
+      pageItems: [2, 5, 10, 20, 50, 100],
     };
   },
   computed: {
     searchItems() {
       return this.headers.filter((item) => item.searchable);
     },
+    //회원일 때는 수정일, 탈퇴회원일 때는 탈퇴일이 보이게 함
+    headers() {
+      if(this.options.mType == 'member') {
+        return this.headersOrigin.filter(item => item.value != 'mb_leave_at')
+      } else {
+        return this.headersOrigin.filter(item => item.value != 'mb_update_at')
+      }
+    },
+    pageCount() {
+      return Math.ceil(this.totalItems / this.options.itemsPerPage);
+    }
   },
   watch: {
     options: {
@@ -91,10 +178,10 @@ export default {
     this.options = this.initOptions();
   },
   mounted() {
-      window.addEventListener('popstate', this.routeChange);
+    window.addEventListener("popstate", this.routeChange);
   },
   destroyed() {
-      window.removeEventListener('popstate', this.routeChange);
+    window.removeEventListener("popstate", this.routeChange);
   },
   methods: {
     initOptions() {
@@ -108,6 +195,7 @@ export default {
         stf: [query.stf || ""],
         stx: [query.stx || ""],
         stc: [query.stc || ""],
+        mType: query.mType || "member",
       };
       return options;
     },
@@ -117,11 +205,22 @@ export default {
     },
     async fetchData() {
       console.log("fetchData call...");
-      if(this.axiosSource) {
-          this.axiosSource.cancel("fetchData 취소");
+      if (this.axiosSource) {
+        this.axiosSource.cancel("fetchData 취소");
       }
       this.loading = true;
-      const payload = { ...this.options };
+      const payload = deepCopy(this.options);
+      if(this.options.mType == 'member') {
+        payload.stf.push('mb_leave_at');
+        payload.stx.push('n');
+        payload.stc.push('null');
+      } else {
+        payload.stf.push('mb_leave_at');
+        payload.stx.push('n');
+        payload.stc.push('not');
+      }
+      delete payload.mType;
+
       const query = qs.stringify(payload);
       this.axiosSource = axios.CancelToken.source();
       try {
@@ -141,27 +240,71 @@ export default {
       }
     },
     pushState() {
-        console.log("pageRouting", this.pageRouting);
-        if (!this.pageRouting) {
-          const opt = {
-            page: this.options.page,
-            itemsPerPage: this.options.itemsPerPage,
-            sortBy: this.options.sortBy[0],
-            sortDesc: this.options.sortDesc[0],
-            stf: this.options.stf[0] || undefined,
-            stc: this.options.stc[0] || undefined,
-            stx: this.options.stx[0] || undefined,
-          };
-          const query = qs.stringify(opt);
-          if (this.pageReady) {
-            history.pushState(null, null, `${this.$route.path}?${query}`);
-          } else {
-            history.replaceState(null, null, `${this.$route.path}?${query}`);
-          }
+      console.log("pageRouting", this.pageRouting);
+      if (!this.pageRouting) {
+        const opt = {
+          page: this.options.page,
+          itemsPerPage: this.options.itemsPerPage,
+          sortBy: this.options.sortBy[0],
+          sortDesc: this.options.sortDesc[0],
+          stf: this.options.stf[0] || undefined,
+          stc: this.options.stc[0] || undefined,
+          stx: this.options.stx[0] || undefined,
+          mType: this.options.mType,
+        };
+        const query = qs.stringify(opt);
+        if (this.pageReady) {
+          history.pushState(null, null, `${this.$route.path}?${query}`);
+        } else {
+          history.replaceState(null, null, `${this.$route.path}?${query}`);
         }
-    }
+      }
+    },
+    openDialog(member) {
+      this.currMember = member;
+      this.$refs.dialog.open();
+    },
+    dialogClose() {
+      this.currMember = null;
+    },
+    async saveMember(form) {
+      // axios 요청
+      this.loading = true;
+      const data = await this.$axios.patch("/api/member", form);
+      this.loading = false;
+      if (data) {
+        const idx = this.items.indexOf(this.currMember);
+        console.log(data);
+        this.items.splice(idx, 1, data);
+        this.$toast.info(`${data.mb_name}님의 정보가 수정되었습니다.`);
+      }
+    },
+    async leaveMember() {
+      const result = await this.$ezNotify.confirm(
+        `${this.currMember.mb_name}님 회원 탈퇴 처리를 하시겠습니까?`,
+        "회원탈퇴",
+        {
+          icon: "mdi-alert",
+        }
+      );
+
+      if (!result) return;
+
+      this.isLoading = true;
+
+      const form = {
+        mb_id: this.currMember.mb_id,
+        mb_leave_at: this.$moment().format("LT"),
+      };
+      const data = await this.$axios.patch("/api/member", form);
+      if (data) {
+        this.$toast.info(`${this.currMember.mb_name}님을 탈퇴 처리하였습니다.`);
+        this.$refs.dialog.close();
+        this.pageRouting = true;
+        this.fetchData();
+      }
+    },
   },
-  
 };
 </script>
 
